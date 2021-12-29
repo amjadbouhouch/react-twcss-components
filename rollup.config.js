ce3ece0519739d7c0c3fdeff182f5bd7324c813c
Bouhouch Amjed <7351600-amjed.winshot@users.noreply.gitlab.com>
2021-12-29 19:52:20 +0100

    fix purge-css not working

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,20 @@
-// import resolve from "@rollup/plugin-node-resolve";
-// import commonjs from "@rollup/plugin-commonjs";
-// import { terser } from "rollup-plugin-terser";
-// import postcss from "rollup-plugin-postcss";
+import postcss from "rollup-plugin-postcss";
 import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import typescript from "@rollup/plugin-typescript";
-
-const packageJson = require("./package.json");
+import { terser } from "rollup-plugin-terser";
+import visualizer from "rollup-plugin-visualizer";
 export default [
   {
     input: "./src/index.ts",
     output: [
       {
-        file: packageJson.main,
-        format: "cjs",
-        // sourcemap: true,
-        name: "react-lib",
+        dir: "dist",
+        format: "esm",
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        sourcemap: true,
       },
     ],
     plugins: [
@@ -24,33 +22,35 @@ export default [
         exclude: "node_module/**",
         presets: ["@babel/preset-react"],
       }),
+      postcss({
+        // extensions: ['css', '.less'],
+        minimize: true,
+        modules: true,
+        use: {
+          sass: null,
+          stylus: null,
+          less: { javascriptEnabled: true },
+        }, //, modifyVars: antdVars }},,
+        extract: true,
+        config: {
+          path: "./postcss.config.js",
+          ctx: null,
+        },
+      }),
       external(),
       resolve(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+
+      terser(),
+      visualizer({
+        filename: "bundle-analysis.html",
+        open: true,
+      }),
+      typescript({
+        tsconfig: "./tsconfig.build.json",
+        declaration: true,
+        declarationDir: "dist",
+      }),
     ],
+    external: ["react", "react-dom", "tailwindcss"],
   },
 ];
-// export default {
-//   input: "src/index.ts",
-//   output: [
-//     {
-//       file: packageJson.main,
-//       format: "cjs",
-//       sourcemap: true,
-//       name: "react-lib",
-//     },
-//     {
-//       file: packageJson.module,
-//       format: "esm",
-//       sourcemap: true,
-//     },
-//   ],
-//   plugins: [
-//     external(),
-//     resolve(),
-//     commonjs(),
-//     typescript({ tsconfig: "./tsconfig.json" }),
-//     postcss(),
-//     terser(),
-//   ],
-// };
